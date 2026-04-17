978947ccf11229621b91f19c27fda8d3272274cf
HackTheOxidation <tomas.hagenau@protonmail.ch>
2026-04-17 08:48:58 +0200

    Update lexer

--- include/Lexer.hpp
+++ include/Lexer.hpp
@@ -19,8 +19,12 @@ public:
 
 private:
   std::string json_;
-};
 
+  void tokenize_null(Tokens &tokens, std::string::const_iterator& iter, std::size_t &line_no);
+  void tokenize_bool(Tokens &tokens, std::string::const_iterator& iter, std::size_t &line_no);
+  void tokenize_number(Tokens &tokens, std::string::const_iterator& iter, std::size_t &line_no);
+  void tokenize_string(Tokens &tokens, std::string::const_iterator& iter, std::size_t &line_no);
+};
 } // namespace picoJSON
 
 #endif
--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,53 +1,87 @@
 #include <Lexer.hpp>
+#include <cctype>
 #include <cstddef>
+#include <iterator>
 #include <sstream>
 
 namespace picoJSON {
 
 Lexer::Lexer(const std::string json) : json_(json) { tokenize(); }
 
-void tokenize_null(Tokens &tokens, std::size_t &line_no) {
-  std::string buffer = "";
-  tokens.push_back(std::pair<Token, std::string>(Token::JSONNULL, buffer));
-  line_no++;
+void Lexer::tokenize_null(Tokens& tokens, std::string::const_iterator& iter, std::size_t& line_no) {
+  const auto dist = std::distance(json_.cbegin(), iter) - 1;
+  std::string buffer = json_.substr(dist, 4);
+  if (buffer == "null") {
+    tokens.push_back(std::pair(Token::JSONNULL, buffer));
+    std::advance(iter, 4);
+  }
 }
 
-void tokenize_bool(Tokens &tokens, std::size_t &line_no) {
-  std::string buffer = "";
-  tokens.push_back(std::pair<Token, std::string>(Token::JSONTRUE, buffer));
-  line_no++;
+void Lexer::tokenize_bool(Tokens& tokens, std::string::const_iterator& iter, std::size_t& line_no) {
+  const auto dist = std::distance(json_.cbegin(), iter) - 1;
+  std::string buffer = json_.substr(dist, 4);
+
+  if (buffer == "true") {
+    tokens.push_back(std::pair(Token::JSONTRUE, buffer));
+    std::advance(iter, 4);
+    return;
+  }
+
+  buffer = json_.substr(dist, 5);
+  if (buffer == "false") {
+    tokens.push_back(std::pair(Token::JSONFALSE, buffer));
+    std::advance(iter, 5);
+    return;
+  }
 }
 
-void tokenize_number(Tokens &tokens, std::size_t &line_no) {
+void tokenize_number(Tokens& tokens, std::string::const_iterator& iter, std::size_t& line_no) {
   std::string buffer = "";
-  tokens.push_back(std::pair<Token, std::string>(Token::NUMBER, buffer));
-  line_no++;
+  while (std::isdigit(*iter)) {
+    buffer += *iter;
+    iter++;
+  }
+
+  if (*iter == '.') {
+    buffer += *(iter++);
+    while (std::isdigit(*iter)) {
+      buffer += *iter;
+      iter++;
+    }
+  }
+
+  tokens.push_back(std::pair(Token::NUMBER, buffer));
 }
 
-void tokenize_string(Tokens &tokens, std::size_t &line_no) {
+void tokenize_string(Tokens& tokens, std::string::const_iterator& iter, std::size_t& line_no) {
   std::string buffer = "";
-  tokens.push_back(std::pair<Token, std::string>(Token::STRING, buffer));
-  line_no++;
+  while (*iter != '\"') {
+    buffer += *iter;
+    iter++;
+  }
+  tokens.push_back(std::pair(Token::STRING, buffer));
 }
 
 Tokens Lexer::tokenize() {
   Tokens tokens{};
-  std::string buffer;
 
   std::size_t line_no = 1;
-  for (char c : json_) {
+  for (auto iter = json_.cbegin(); iter != json_.cend(); iter++) {
+    c = *iter;
     if (c == '{') {
-      tokens.push_back(std::pair<Token, std::string>(Token::LCURLY, buffer));
+      tokens.push_back(std::pair(Token::LCURLY, "{"));
     } else if (c == '[') {
-      tokens.push_back(std::pair<Token, std::string>(Token::LBRACE, buffer));
+      tokens.push_back(std::pair(Token::LBRACE, "["));
     } else if (c == '(') {
-      tokens.push_back(std::pair<Token, std::string>(Token::LPAREN, buffer));
+      tokens.push_back(std::pair(Token::LPAREN, "("));
     } else if (c == ',') {
-      tokens.push_back(std::pair<Token, std::string>(Token::NUMBER, buffer));
+      tokens.push_back(std::pair(Token::COMMA, ","));
     } else if (c == ':') {
-      tokens.push_back(std::pair<Token, std::string>(Token::COLON, buffer));
+      tokens.push_back(std::pair(Token::COLON, ":"));
     } else if (c == '\"') {
-      tokenize_string(tokens, line_no);
+      tokenize_string(tokens, ++iter, line_no);
+    } else if (std::isdigit(c)) {
+      tokenize_number(tokens, iter, line_no);
     } else {
       // Raise exception.
     }
